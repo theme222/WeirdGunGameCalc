00f3da45c820ce80e276449319b7cd3ad4048e72
Sira Tongsima <sira.tongsima@yahoo.com>
2025-06-02 13:01:59 +0700

    Update Commands.sh

--- Commands.sh
+++ Commands.sh
@@ -1,6 +1,6 @@
 # This are commands for my setup specifically. You can config these example commands and run them in any order you want.
 g++ -Iinclude -std=c++20 Calculator.cpp -o Calculator &&
-python FileFormatter.py &&
+python FileFormatter.py && # This doesn't need to be re ran every time because it saves it's results in Data/FullData.json already.
 ./Calculator -o InterestingBuilds/FastestTTKSMG.txt -i SMG AR LMG --magazineMin 30 --speedMin 0 --damageMax 99.99 --spreadAimMax 1.049 --recoilAimMax 35 &&
 ./Calculator -o InterestingBuilds/FastestTTKNonSMG.txt -i AR LMG --magazineMin 30 --speedMin 0 --damageMax 99.99 --spreadAimMax 0.649 --recoilAimMax 30 &&
 ./Calculator -o InterestingBuilds/FastestHeadshotSniper.txt -i Sniper --speedMin 0 --damageMin 49.95 --spreadAimMax 0.149 --sort FIRERATE &&
